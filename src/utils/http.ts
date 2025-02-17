f82a86915081eb318873916bec4a13273dac853d
Tsan Tri Duc <tsantriduc@gmail.com>
2025-02-17 15:43:36 +0700

    chang PORT

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,7 +4,7 @@ class Http {
   instance: AxiosInstance
   constructor() {
     this.instance = axios.create({
-      baseURL: 'http://localhost:8080/',
+      baseURL: 'http://localhost:5703/',
       timeout: 3000,
       headers: {
         'Content-Type': 'application/json'
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080
+    port: 5703
   },
   css: {
     devSourcemap: true
